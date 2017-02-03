e4d52fadbc396331af4ab5cbc59505a7d0433b87
tannerkrewson <tannerkrewson@gmail.com>
2017-02-02 22:56:33 -0600

    Added calorie-countdown

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$("#start-button").click(function () {
+  // get the user's inputted total calorie number from the textbox
+  var totalCaloriesFromTextBox = parseInt($("#total-calories").val());
+
+  if (totalCaloriesFromTextBox > 0) {
+    setCalories(totalCaloriesFromTextBox);
+    $(".start-page").hide();
+    $(".calculator-page").show();
+  }
+
+});
+
+$("#add-button").click(function () {
+  // get the user's inputted cal count for this plate from the textbox
+  var plateCaloriesFromTextBox = $("#plate-calories").val();
+
+  //clear the number they entered
+  $("#plate-calories").val("");
+
+  //check if it is a valid number
+  if (!isNaN(plateCaloriesFromTextBox) && plateCaloriesFromTextBox !== "") {
+    subtractCalories(parseInt(plateCaloriesFromTextBox));
+  }
+});
+
+$("#reset-button").click(function () {
+  $(".calculator-page").hide();
+  $(".start-page").show();
+});
+
+$(document).ready(function(){
+    $('#total-calories').keypress(function(e){
+      if(e.keyCode==13)
+      $('#start-button').click();
+    });
+    $('#plate-calories').keypress(function(e){
+      if(e.keyCode==13)
+      $('#add-button').click();
+    });
+});
+
+var caloriesLeft = 0;
+function setCalories (newCalorieAmount) {
+  caloriesLeft = newCalorieAmount;
+
+  //display that number in the counter
+  $("#calories-left").text(caloriesLeft);
+}
+
+function subtractCalories (amountToSubtract) {
+  setCalories(caloriesLeft - amountToSubtract);
+}
